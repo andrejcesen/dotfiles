d93d1124cc761d658ae6c2ab902146b99cb5acea
Andrej Česen <andrej.cesen@me.com>
2018-06-10 23:22:34 +0200

    Added Android dev exports

--- .config/fish/exports.fish
+++ .config/fish/exports.fish
@@ -11,3 +11,8 @@ set -x MANPAGER "less -X"
 
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
 # set -x NVM_DIR "$HOME/.nvm"
+
+# Android (test to avoid warnings on nonexisting paths)
+set -x ANDROID_HOME "$HOME/Library/Android/sdk"
+test -d "$ANDROID_HOME/tools"; and set -x PATH "$ANDROID_HOME/tools" $PATH
+test -d "$ANDROID_HOME/platform-tools"; and set -x PATH "$ANDROID_HOME/platform-tools" $PATH
