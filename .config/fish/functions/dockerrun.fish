d178544cec1797911eaf4dafb2400fa58234c0cf
Andrej Česen <andrej.cesen@me.com>
2019-06-02 18:42:02 +0200

    Updated unifi docker parameters

--- .config/fish/functions/dockerrun.fish
+++ .config/fish/functions/dockerrun.fish
@@ -14,8 +14,10 @@ function dockerrun --description 'Run Docker Container'
       docker run -d --init \
         --name unifi \
         -p 8080:8080 -p 8443:8443 -p 3478:3478/udp -p 10001:10001/udp \
-        -e RUNAS_UID0='false' \
         -e TZ='Europe/Ljubljana' \
+        -e RUNAS_UID0='false' \
+        -e UNIFI_UID=(id -u) \
+        -e UNIFI_GID=(id -g) \
         -v ~/unifi:/unifi \
           jacobalberty/unifi:stable
 
