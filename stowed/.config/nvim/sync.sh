f1c5b4f17c8b37621c1c2e96d92dd4771f8b5e3b
Andrej Česen <andrej.cesen@me.com>
2022-04-26 09:31:10 +0200

    Sync tree-sitter modules each time
    Updated with Olical's dotfiles (34829b1)

--- stowed/.config/nvim/sync.sh
+++ stowed/.config/nvim/sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CONFIG=$HOME/.config/nvim
-mkdir -p $CONFIG/undo $CONFIG/spell $CONFIG/autoload
-rm -rf $CONFIG/lua
+mkdir -p "$CONFIG/undo" "$CONFIG/spell" "$CONFIG/autoload"
+rm -rf "$CONFIG/lua"
 nvim +qa
-nvim +"au User PackerComplete qa" +PackerSync
+nvim +"au User PackerComplete qa" +PackerSync +TSUpdateSync
