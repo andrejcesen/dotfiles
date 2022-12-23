7b5ec8da57dc465b579f3e1c950d63c0fd2a4d85
Andrej Česen <andrej.cesen@me.com>
2022-12-23 10:43:46 +0100

    Add Bear notes backup

--- script/backup-old-machine.sh
+++ script/backup-old-machine.sh
@@ -1,6 +1,7 @@
+# Best to run this manually.
+
 # Backup old machine's key items
 mkdir -p ~/migration/home/
-mkdir -p ~/migration/Library/"Application Support"/
 
 # Backup some dotfiles likely not under source control
 cp -Rp \
@@ -17,9 +18,15 @@ cp -Rp \
   ~/Projects \
     ~/migration
 
+mkdir -p ~/migration/Library/
 cp -Rp ~/Library/Fonts ~/migration/Library/ # all those fonts you've installed
 cp -Rp ~/Library/Spelling/*.{aff,dic} ~/migration/Library/Spelling # spelling dictionaries
 
-# Editor settings & plugins
-cp -Rp ~/Library/Application\ Support/Code* ~/migration/Library/"Application Support"
+# Backup Bear notes
+mkdir -p "$HOME/migration/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/" 
+cp -Rp   "$HOME/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data" \
+         "$HOME/migration/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/"
 
+# Editor settings & plugins
+mkdir -p ~/migration/Library/"Application Support"/
+cp -Rp ~/Library/Application\ Support/Code* ~/migration/Library/"Application Support"
