d989ebe554d3b618b800b725f433aa130472163f
Andrej Česen <andrej.cesen@me.com>
2023-04-05 09:24:50 +0200

    [fish] Merge emacs with vi keybindings

--- stowed/.config/fish/functions/fish_user_key_bindings.fish
+++ stowed/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,12 @@
 function fish_user_key_bindings
-  fish_vi_key_bindings
+  # Execute this once per mode that emacs bindings should be used in
+  fish_default_key_bindings -M insert
+
+  # Then execute the vi-bindings so they take precedence when there's a conflict.
+  # Without --no-erase fish_vi_key_bindings will default to
+  # resetting all bindings.
+  # The argument specifies the initial mode (insert, "default" or visual).
+  fish_vi_key_bindings --no-erase insert
 
   if test -e "$(brew --prefix)/opt/fzf/shell/key-bindings.fish" >/dev/null
     source "$(brew --prefix)/opt/fzf/shell/key-bindings.fish"
@@ -10,7 +17,6 @@ function fish_user_key_bindings
   end
 
   for mode in insert default visual
-    bind -M $mode \cf forward-char # Enable <C-F> in vi-bindings.
     # If a script produces output, it should finish by calling
     # `commandline -f repaint` to tell fish that a repaint is in order.
     bind -M $mode \co 'tmux-sessionizer; commandline -f repaint'
