1a0e681321012439f4422f0b0e679209808fb28b
Andrej Česen <andrej.cesen@me.com>
2023-09-06 09:43:10 +0200

    Update backup script

--- script/backup-old-machine.sh
+++ script/backup-old-machine.sh
@@ -17,20 +17,23 @@ cd ~ || exit
 rsync -R .local/state/nvim/shada/main.shada ~/migration/home
 rsync -R .local/share/fish/fish_history ~/migration/home
 
-cp -Rp \
-  ~/Documents \
-  ~/Projects \
-    ~/migration
-
 mkdir -p ~/migration/Library/
 cp -Rp ~/Library/Fonts ~/migration/Library/ # all those fonts you've installed
 cp -Rp ~/Library/Spelling/*.{aff,dic} ~/migration/Library/Spelling # spelling dictionaries
 
-# Backup Bear notes
+# 1. Make sure to close Bear app.
+# 2. Backup Bear notes
 mkdir -p "$HOME/migration/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/" 
 cp -Rp   "$HOME/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data" \
          "$HOME/migration/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/"
+# 3. Also backup to .bear2bk file in ~/Documents/2-Areas
+shortcuts run "Backup Bear Notes"
 
 # Editor settings & plugins
 mkdir -p ~/migration/Library/"Application Support"/
 cp -Rp ~/Library/Application\ Support/Code* ~/migration/Library/"Application Support"
+
+cp -Rp \
+  ~/Documents \
+  ~/Projects \
+    ~/migration
