7bc443327bad5c6e86c2ec9e336d685345424809
Andrej Česen <andrej.cesen@me.com>
2026-03-23 09:10:30 +0100

    Backup ~/Library/Services

--- script/backup-old-machine.sh
+++ script/backup-old-machine.sh
@@ -15,10 +15,11 @@ cp -Rp \
     ~/migration/home
 
 cd ~ || exit
-rsync -R .local/bin_private/aws-rotate.sh ~/migration/home
-rsync -R .local/state/nvim/shada/main.shada ~/migration/home
-rsync -R .local/share/fish/fish_history ~/migration/home
-rsync -R .m2/settings.xml ~/migration/home
+rsync -aR .local/bin_private/aws-rotate.sh ~/migration/home
+rsync -aR .local/state/nvim/shada/main.shada ~/migration/home
+rsync -aR .local/share/fish/fish_history ~/migration/home
+rsync -aR .m2/settings.xml ~/migration/home
+rsync -aR Library/Services ~/migration/home
 
 mkdir -p ~/migration/Library/
 cp -Rp ~/Library/Fonts ~/migration/Library/ # all those fonts you've installed
